07e13dd890cdb286578eaf5abaa31e36f1ddc5ce
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2020-09-13 02:27:29 +0530

    unlimited no of bytes print statement

--- print/print.go
+++ print/print.go
@@ -5,5 +5,5 @@ import "fmt"
 func main() {
 	fmt.Print("Hello")            //This print will not include a new line after the execution is done.
 	fmt.Println("Hello")          //This println will include a new line after the execution is done.
-	fmt.Println("Hello", 2, true) //This println will include a new line after the execution is done.
+	fmt.Println("Hello", 2, true) //Unlimited no of bytes taken
 }
